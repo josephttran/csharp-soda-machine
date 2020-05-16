0c5d0d635e1d84ed2be8f4d81568e87f1f534321
josephttran <42635012+josephttran@users.noreply.github.com>
2020-05-16 16:05:05 -0700

    Implement soda inventory page

--- SodaMachineWebApplication/Pages/Admin/SodaInventory/Index.cshtml.cs
+++ SodaMachineWebApplication/Pages/Admin/SodaInventory/Index.cshtml.cs
@@ -1,11 +1,10 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using SodaMachineLibrary.DataAccess;
+using SodaMachineLibrary.Models;
 
 namespace SodaMachineWebApplication.Pages.Admin.SodaInventory
 {
@@ -19,8 +18,86 @@ namespace SodaMachineWebApplication.Pages.Admin.SodaInventory
             _dataAccess = dataAccess;
         }
 
-        public void OnGet()
+        public string Message { get; set; }
+        [BindProperty]
+        public string SelectedSoda { get; set; }
+        [BindProperty]
+        public string SelectedSodaQuantity { get; set; }
+        public SelectList SodaAddList { get; set; }
+        public Dictionary<string, int> SodaQuantity { get; set; }
+
+        public IActionResult OnGet()
         {
+            SodaQuantity = new Dictionary<string, int>();
+
+            List<SodaModel> sodaInventory = _dataAccess.SodaInventoryGetAll();
+
+            foreach (SodaModel sodaModel in sodaInventory)
+            {
+                string sodaKey = $"{ sodaModel.Name } { sodaModel.SlotOccupied }";
+
+                if (SodaQuantity.ContainsKey(sodaKey))
+                {
+                    SodaQuantity[sodaKey]++;
+                }
+                else
+                {
+                    SodaQuantity[sodaKey] = 1;
+                }
+            }
+
+            SodaAddList = new SelectList(SodaQuantity, "Key", "Key");
+
+            return Page();
+        }
+
+        public IActionResult OnPostAddSoda()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            if (string.IsNullOrEmpty(SelectedSoda))
+            {
+                Message = "Please select soda with slot";
+            }
+            else
+            {
+                if (int.TryParse(SelectedSodaQuantity, out int quantity))
+                {
+                    if (quantity > 0 && quantity < 10)
+                    {
+                        List<SodaModel> sodas = new List<SodaModel>();
+                        string[] sodaNameSlot = SelectedSoda.Split(" ");
+
+                        for (int i = 0; i < quantity; i++)
+                        {
+                            SodaModel sodaModel = new SodaModel
+                            {
+                                Name = sodaNameSlot[0],
+                                SlotOccupied = sodaNameSlot[1]
+                            };
+
+                            sodas.Add(sodaModel);
+                        }
+
+                        _dataAccess.SodaInventoryAddSodas(sodas);
+
+                        Message = $"Successfully added { quantity } { sodaNameSlot[0] } at slot { sodaNameSlot[1] }";
+                    }
+                    else
+                    {
+                        Message = "Quantity must be greater than 0 and less than 10";
+                    }
+                }
+                else
+                {
+                    Message = "Please enter a number for quantity";
+                }
+            }
+
+            return OnGet();
         }
     }
 }
