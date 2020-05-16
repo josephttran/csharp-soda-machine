efc41ca54a3983847359353c3dea6a906d32fb3f
josephttran <42635012+josephttran@users.noreply.github.com>
2020-05-16 16:09:08 -0700

    Implement income page

--- SodaMachineWebApplication/Pages/Admin/Income/Index.cshtml.cs
+++ SodaMachineWebApplication/Pages/Admin/Income/Index.cshtml.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -19,8 +15,29 @@ namespace SodaMachineWebApplication.Pages.Admin.Income
             _dataAccess = dataAccess;
         }
 
-        public void OnGet()
+        [BindProperty]
+        public decimal CurrentIncome { get; set; }
+        [BindProperty]
+        public decimal TotalIncome { get; set; }
+
+        public IActionResult OnGet()
         {
+            CurrentIncome = _dataAccess.MachineInfoCashOnHand();
+            TotalIncome = _dataAccess.MachineInfoTotalIncome();
+
+            return Page();
+        }
+
+        public IActionResult OnPostEmptyMachine()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _dataAccess.MachineInfoEmptyCash();
+
+            return OnGet();
         }
     }
 }
